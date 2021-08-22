491bc95c77d63654ef42c3d687380179ac8ad9a4
kajmaj87 <kajmaj87@gmail.com>
2021-08-22 19:01:04 +0200

    Added force fields

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,42 @@ use bevy::prelude::*;
 fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
+    commands.spawn().insert(ForceField{
+        field: |pos| {
+            -pos.distance_squared(Vec2::ZERO)/1_000.0 * pos.signum()
+        }
+    });
 }
 
 const PARTICLE_SCALE: f32 = 3.0;
 
 struct ParticleSpawnEvent(Vec2);
+
+struct Velocity(Vec2);
+struct ForceField {
+    field: fn(Vec2) -> Vec2,
+}
+
+fn update_positions(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
+    for (mut position, velocity) in query.iter_mut() {
+        position.translation = position.translation + Vec3::new(velocity.0.x, velocity.0.y, 0.0) * time.delta_seconds();
+    }
+}
+
+fn update_velocity(
+    mut particles: Query<(&Transform, &mut Velocity)>,
+    mut force_fields: Query<(&ForceField,)>,
+    time: Res<Time>,
+) {
+    for (position, mut velocity) in particles.iter_mut() {
+        for (force_field,) in force_fields.iter_mut() {
+            let position = position.translation;
+            let update = (force_field.field)(Vec2::new(position.x, position.y)) * time.delta_seconds();
+            velocity.0 += update;
+            println!("Updated by {}", update);
+        }
+    }
+}
 
 fn particle_spawner(
     mut commands: Commands,
@@ -17,15 +48,17 @@ fn particle_spawner(
 ) {
     for e in spawner.iter() {
         let texture_handle = asset_server.load("rolly_happy.png");
-        commands.spawn_bundle(SpriteBundle {
-            material: materials.add(texture_handle.into()),
-            transform: Transform {
-                translation: Vec3::new(e.0.x, e.0.y, 0.0),
-                scale: Vec3::splat(PARTICLE_SCALE),
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.add(texture_handle.into()),
+                transform: Transform {
+                    translation: Vec3::new(e.0.x, e.0.y, 0.0),
+                    scale: Vec3::splat(PARTICLE_SCALE),
+                    ..Default::default()
+                },
                 ..Default::default()
-            },
-            ..Default::default()
-        });
+            })
+            .insert(Velocity(Vec2::new(0.0, 0.0)));
     }
 }
 
@@ -53,5 +86,7 @@ fn main() {
         .add_event::<ParticleSpawnEvent>()
         .add_system(mouse_handler.system())
         .add_system(particle_spawner.system())
+        .add_system(update_positions.system())
+        .add_system(update_velocity.system())
         .run();
 }
