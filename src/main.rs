64aed3e4abf4719ad3ad7ec22aec422c0fcd1597
kajmaj87 <kajmaj87@gmail.com>
2021-08-22 17:10:03 +0200

    Added mouse handling and adding of particle

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,45 @@
 use bevy::prelude::*;
 
-struct Person;
-struct Name(String);
-pub struct HelloPlugin;
-
-struct GreetTimer(Timer);
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
-    }
+fn setup(mut commands: Commands){
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
 }
 
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Elaina Proctor".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Renzo Hume".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Zayna Nieves".to_string()));
-}
+const PARTICLE_SCALE: f32 = 3.0;
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
+fn mouse_handler(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mouse_button_input: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let window = windows
+        .get_primary()
+        .expect("Primary window does not exist!");
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        let texture_handle = asset_server.load("rolly_happy.png");
+        commands.spawn_bundle(SpriteBundle {
+            material: materials.add(texture_handle.into()),
+            transform: Transform {
+                translation: Vec3::new(
+                    window.cursor_position().unwrap().x - window.width()/2.0,
+                    window.cursor_position().unwrap().y - window.height()/2.0,
+                    0.0,
+                ),
+                scale: Vec3::splat(PARTICLE_SCALE),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+        println!("click at {:?}", window.cursor_position());
     }
 }
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup.system())
+        .add_system(mouse_handler.system())
         .run();
 }
