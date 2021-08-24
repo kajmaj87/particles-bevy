0ba7c267001e70d9400d11a1461b141200bcf31f
kajman <kajmaj87@gmail.com>
2021-08-24 15:26:08 +0200

    Added simulation speed setting

--- src/physics.rs
+++ src/physics.rs
@@ -1,8 +1,14 @@
-use bevy::prelude::*;
+use bevy::{
+    core::{FixedTimestep, FixedTimesteps},
+    prelude::*,
+};
 
 use crate::{Materials, ParticleCounter, ParticleSpawnEvent, ParticleType};
 
 const PARTICLE_SCALE: f32 = 4.0;
+const SIMULATION_LOOPS: u32 = 1;
+// const FPS: f32 = 60.0;
+// const RUNS_PER_SECOND: f32 = FPS * SIMULATION_SPEED;
 pub struct PhysicsPlugin;
 
 struct Velocity(Vec2);
@@ -10,12 +16,39 @@ struct ForceField {
     field: fn(Vec2, Vec2) -> Vec2,
 }
 
+struct PhysicsStage(SystemStage);
+
+impl Stage for PhysicsStage {
+    fn run(&mut self, world: &mut World) {
+        println!("Running Physics stage");
+        for _ in 0..SIMULATION_LOOPS {
+            println!("Running simulation frame");
+            self.0.run(world);
+        }
+    }
+}
+
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_startup_system(setup.system())
             .add_system(particle_spawner.system())
-            .add_system(update_positions.system())
-            .add_system(update_velocity.system());
+            .add_stage_after(
+                CoreStage::Update,
+                "physics",
+                PhysicsStage(SystemStage::parallel()
+                    .with_system(update_positions.system())
+                    .with_system(update_velocity.system())),
+            );
+        // .add_system(simulation_system.system());
+        // .add_system_set(;
+        //     SystemSet::new()
+        //         .with_run_criteria(
+        //             FixedTimestep::steps_per_second(RUNS_PER_SECOND.into()).with_label("fixed"),
+        //         )
+        //         .with_system(update_positions.system())
+        //         .with_system(update_velocity.system())
+        //         // .with_system(fixed_update.system()),
+        // );
     }
 }
 
@@ -28,7 +61,31 @@ fn setup(mut commands: Commands) {
         .insert(Transform::default());
 }
 
+// fn simulation_system(
+//     positions_query: &Query<(&mut Transform, &Velocity)>,
+//     velocity_query: Query<(Entity, &Transform, &mut Velocity)>,
+//     force_query: Query<(Entity, &Transform, &ForceField)>,
+//     time: Res<Time>,
+// ) {
+//     for i in 0..SIMULATION_LOOPS {
+//         update_positions(
+//             positions_query,
+//             time.delta_seconds() / SIMULATION_SPEED,
+//         );
+//         update_velocity(
+//             velocity_query,
+//             force_query,
+//             time.delta_seconds() / SIMULATION_SPEED,
+//         );
+//     }
+// }
+
 fn update_positions(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
+    println!("Realtime  delta t: {}", time.delta_seconds());
+    println!(
+        "Simulated delta t: {}",
+        time.delta_seconds() * SIMULATION_LOOPS as f32
+    );
     for (mut position, velocity) in query.iter_mut() {
         position.translation = position.translation
             + Vec3::new(velocity.0.x, velocity.0.y, 0.0) * time.delta_seconds();
@@ -46,11 +103,27 @@ fn update_velocity(
             if particle_ent != force_ent {
                 let position = Vec2::from(position.translation);
                 let center = Vec2::from(force_center.translation);
-                let update = (force_field.field)(position, center) * time.delta_seconds();
+                let update =
+                    (force_field.field)(position, center) * time.delta_seconds();
                 velocity.0 += update;
             }
         }
     }
+}
+
+fn fixed_update(mut last_time: Local<f64>, time: Res<Time>, fixed_timesteps: Res<FixedTimesteps>) {
+    println!(
+        "fixed_update: {}",
+        time.seconds_since_startup() - *last_time,
+    );
+
+    let fixed_timestep = fixed_timesteps.get("fixed").unwrap();
+    println!(
+        "  overstep_percentage: {}",
+        fixed_timestep.overstep_percentage()
+    );
+
+    *last_time = time.seconds_since_startup();
 }
 
 fn particle_spawner(
